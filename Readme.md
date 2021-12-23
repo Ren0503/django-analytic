7bc8b359b0e4c9d2133bf25b068779f95fa4864f
Ren0503 <illuminati0440@gmail.com>
2021-12-23 09:05:06 +0700

    Up

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Analytic with Django
+
+![screenshots](./screenshots.png)
+
+## How to run the project
+To run the Django development server, execute the following command:
+
+`python manage.py runserver`
+
+Next, open the following URL in your browser:
+
+`http://127.0.0.1:8000/dashboard/`
+
+## License
+
+[veronikaro](https://github.com/veronikaro)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+from dashboard.models import Order
 
 # Create your views here.
+def dashboard_with_pivot(request):
+    return render(request, 'dashboard_with_pivot.html', {})
+
+
+def pivot_data(request):
+    dataset = Order.objects.all()
+    data = serializers.serialize('json', dataset)
+    return JsonResponse(data, safe=False)
